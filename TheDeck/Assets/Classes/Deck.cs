790bc57bb31871bdac4486c8866311c1cdf0fc3b
Bingo00000000 <53280532+Bingo00000000@users.noreply.github.com>
2020-07-26 11:57:40 -0700

    Update Deck.cs
    add the set of cards in a deck as a stack

--- TheDeck/Assets/Classes/Deck.cs
+++ TheDeck/Assets/Classes/Deck.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Deck : MonoBehaviour
-{
+{   //the set of cards in a deck 
+    private Stack Set = new Stack();
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
