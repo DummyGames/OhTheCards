055a483d444d2ed0ec8606a3484702c009f78eae
Bingo00000000 <53280532+Bingo00000000@users.noreply.github.com>
2020-07-26 12:27:54 -0700

    Update Deck.cs
    commented out the double for loop

--- TheDeck/Assets/Classes/Deck.cs
+++ TheDeck/Assets/Classes/Deck.cs
@@ -8,7 +8,12 @@ public class Deck : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       /* for (int i = 0; i < 4; i++) {
+            for (int i = 2; i < 14; i++)
+            {
+                Set.Push(new GameObject("Card"));
+            }
+        }*/
     }
 
     
