5a3d806507c4e3b253cd555611a71aaa5526577c
Ellienn Tatar <ellienntatar@outlook.com>
2020-08-23 13:55:08 -0700

    Add InputController class

---
+++ TheDeck/Assets/Classes/InputController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    float yPos;
+    float xPos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //primary mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            xPos = mousePos.x;
+            yPos = mousePos.y;
+            UnityEngine.Debug.Log("Mouse x: " + xPos + ", Mouse Y: " + yPos);
+        }
+    }
+}
