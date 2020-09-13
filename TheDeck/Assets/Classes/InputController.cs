d55b6ce556c21871f98de72ee6488a5b07a14e78
Ellienn Tatar <ellienntatar@outlook.com>
2020-09-13 14:22:27 -0700

    Allow for cards to be drawn only when clicking on deck

--- TheDeck/Assets/Classes/InputController.cs
+++ TheDeck/Assets/Classes/InputController.cs
@@ -22,11 +22,26 @@ public class InputController : MonoBehaviour
         //primary mouse click
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Input.mousePosition;
-            xPos = mousePos.x;
-            yPos = mousePos.y;
-            UnityEngine.Debug.Log("Mouse x: " + xPos + ", Mouse Y: " + yPos);
-            deckScript.Draw();
+            if (DetectDeck())
+            {
+                Vector3 mousePos = Input.mousePosition;
+                xPos = mousePos.x;
+                yPos = mousePos.y;
+                UnityEngine.Debug.Log("Mouse x: " + xPos + ", Mouse Y: " + yPos);
+                deckScript.Draw();
+            }
         }
+    }
+
+    public bool DetectDeck() {
+        //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.Equals(theDeck))
+                return true;
+        }
+        return false;
     }
 }
