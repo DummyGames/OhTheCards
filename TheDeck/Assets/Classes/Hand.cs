6d26cbd6015fade3b686107ec9fbcdbec061c6ca
Ellienn Tatar <elliennshouse@outlook.com>
2021-02-21 13:05:32 -0800

    Handle rotation when a card enters hand area

---
+++ TheDeck/Assets/Classes/Hand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Card"))
+        {
+            GameObject card = other.gameObject;
+            card.transform.rotation = Quaternion.Euler(260, 0, 0);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Card"))
+        {
+            GameObject card = other.gameObject;
+            card.transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+    }
+}
