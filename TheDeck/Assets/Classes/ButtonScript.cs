e015ea11eb05375ba3aa011aba6fb409e77ba101
Bingo <bingo.dc19@gmail.com>
2021-02-28 13:21:47 -0800

    add pause button

---
+++ TheDeck/Assets/Classes/ButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+	public Button yourButton;
+	public GameObject menu;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		menu.SetActive(true);
+	}
+}
